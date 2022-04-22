d4b4dc5cb2eb0e21244b2f989b08f461b9f6e52d
thanos <escelles@gmail.com>
2022-04-22 14:52:59 +0200

    update formuliare contact

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('signup/', signup, name="signup"),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name="logout"),
-    path('dashboard/', dashboard, name='dashboard')
+    path('dashboard/', dashboard, name='dashboard') 
 ]   
